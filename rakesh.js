d17295d4e1c9e231c86bb3e7c58363b214a6ef77
Rakesh Pavan <110276813+rakeshtadi@users.noreply.github.com>
2025-03-10 15:51:27 +0530

    const object using js

---
+++ rakesh.js
@@ -0,0 +1,18 @@
+const product = {
+    title: "CLASSMATE",
+    rating: 4,
+    offer: 4,
+    price:10,
+};
+
+console.log(product);
+
+const profile ={
+    username: "Rakesh",
+    isFolow:true,
+    post:200,
+    followers:5000,
+    following:0,
+    occupation:"Entreprenur",
+};
+console.log(typeof profile.username);
